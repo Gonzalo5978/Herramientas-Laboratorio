889c6ed02254b7617f8459c8726236110858c658
Gonzalo5978 <45925516+Gonzalo5978@users.noreply.github.com>
2023-10-05 10:05:58 +0200

    Add files via upload

--- labo.sh
+++ labo.sh
@@ -34,11 +34,14 @@ trap ctrl_c INT
 #----------------
 
 function revision_errores(){
+
+	clear
+	
 	#Banner
 	toilet Revisor -f pagga.tlf --metal
 
 	#Preguntas info datos
-	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el rango de la instalación [192.xxx.xx]: ${finColor}" && read rango
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el rango de la instalación [192.xxx.xxx]: ${finColor}" && read rango
 	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el Nº de Host de la 1º camara: ${finColor}" && read camara_1
 	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el Nº de Host de la última camara: ${finColor}" && read camara_final
 	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce la password de la instalacion: ${finColor}" && read password
@@ -133,6 +136,9 @@ function revision_errores(){
 #----------------
 
 function repetir_revision(){
+
+	clear
+
 	#Variables extraidas de ejecución anterior
 	rango="$(cat tmp/rango.txt)"
 	camara_1="$(cat tmp/camara_1.txt)"
@@ -225,6 +231,9 @@ function repetir_revision(){
 
 
 function revision_individual(){
+
+	clear
+
 	#Preguntas
 	echo -en "\n${turquesa}[+]${finColor} ${blanco}Intoduce la IP del dispositivo: ${finColor}" && read ip
 	echo -en "${turquesa}[+]${finColor} ${blanco}Intoduce la password: ${finColor}" && read password
@@ -279,73 +288,107 @@ function revision_individual(){
 
 }
 
-
+#-------------------------------------------------
 #---------
 #CORRECTOR
 #---------
 
+#-------------
+#CORREGIR HORA
+#-------------
+
+function time_correct(){
+	echo -en "\n${morado}[?]${finColor} ${blanco}¿Corregir múltiples dispositivos? ${finColor}${turquesa}[Y/N]${finColor}${blanco}:${finColor} " && read sino
+	
+	#Condicional en el parámetro 1 para corregir 1 dispositivo o más (respuesta y/n)
+
+	#----------------------
+	#MODO CAMARAS MULTIPLES
+	#----------------------
+
+	if [[ "$sino" == "Y" || "$sino" == "y" ]]; then
+
+		#Preguntas datos dispositivos
+		echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el rango de la instalación [192.xxx.xx]: ${finColor}" && read rango
+		echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el Nº de Host de la 1º camara: ${finColor}" && read camara_1
+		echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el Nº de Host de la última camara: ${finColor}" && read camara_final
+		echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce la password de la instalacion: ${finColor}" && read password
+		echo -e "\n${blanco}-------------------------------------------------------------------${finColor}\n"
+		
+		#Corrección de datos
+		for camaras in $(seq $camara_1 $camara_final); do
+			
+			timeout 1 ping -c 1 $rango.$camaras >/dev/null
+
+			if [ "$?" -eq 0 ]; then
+
+				curl -X POST -d "@xml_parameters/XML_NTPServer.xml" -H "Content-Type: application/xml" http://$rango.$camaras/ISAPI/System/time/ntpServers --digest -u "admin:$password" -s >/dev/null
+	
+				curl -X PUT -d "@xml_parameters/XML_Time.xml" -H "Content-Type: application/xml" http://$rango.$camaras/ISAPI/System/time --digest -u "admin:$password" -s >/dev/null
+
+				nombres="$(curl http://$rango.$camaras/ISAPI/System/deviceInfo --digest -u "admin:$password" -s | grep "<deviceName>" | sed 's/<deviceName>/[+] Camara: /' | sed 's/<\/deviceName>/- CORREGIDA/')"
+				echo -e "${verde}$nombres${finColor}"
+
+					else
+
+				echo -e "${rojo}[!] Camara: $rango.$camaras - OFFLINE${finColor}"
+			fi
+
+		done
+	
+	#MODO CAMARA ÚNICA
+	elif [[ "$sino" == "N" || "$sino" == "n" ]]; then
+		
+		echo -en "\n${turquesa}[+]${finColor} ${blanco}Intoduce la IP del dispositivo: ${finColor}" && read ip
+		echo -en "${turquesa}[+]${finColor} ${blanco}Intoduce la password: ${finColor}" && read password
+		echo -e "\n${blanco}-------------------------------------------------------------------${finColor}\n"
+
+		timeout 1 ping -c 1 $ip >/dev/null
+
+		if [ "$?" -eq 0 ]; then
+
+			curl -X POST -d "@xml_parameters/XML_NTPServer.xml" -H "Content-Type: application/xml" http://$ip/ISAPI/System/time/ntpServers --digest -u "admin:$password" -s >/dev/null
+			
+			curl -X PUT -d "@xml_parameters/XML_Time.xml" -H "Content-Type: application/xml" http://$ip/ISAPI/System/time --digest -u "admin:$password" -s >/dev/null
+
+			nombres="$(curl http://$ip/ISAPI/System/deviceInfo --digest -u "admin:$password" -s | grep "<deviceName>" | sed 's/<deviceName>/[+] Camara: /' | sed 's/<\/deviceName>/- CORREGIDA/')"
+			echo -e "${verde}$nombres${finColor}" 					
+
+		else
+			echo -e "${rojo}[!] Camara: $ip - OFFLINE${finColor}"
+		fi
+	else
+		echo -e "\n${rojo}[!] ERROR [!]${finColor}"
+	fi
+
+
+}
+
+#------------------
+#SELECTOR CORRECTOR
+#------------------
+
 function corrector(){
+
+	clear
+
 	toilet Corrector -f pagga.tlf --metal
 
 	#Pregunta de parámetro a corregir
-	echo -e "\n${morado}[!]${finColor} ${blanco}Que quieres corregir?${finColor} ${morado}[!]${finColor}\n"
+	echo -e "\n${morado}[!]${finColor} ${blanco}¿Que quieres corregir?${finColor} ${morado}[!]${finColor}\n"
 	
-	echo -e "\t${turquesa}[1]${finColor} ${blanco}NTP${finColor}"
+	echo -e "\t${turquesa}[1]${finColor} ${blanco}Hora${finColor}"
 
 	echo -en "\n${morado}[+]${finColor} ${blanco}Introduce el parámetro a corregir: ${finColor}" && read parameter
 	
 	#IF para seleccionar parámetro
 	
+	#--------------
 	#CORRECCIÓN NTP
+	#--------------
+	
 	if [ "$parameter" -eq 1 ]; then
-		echo -en "\n${morado}[?]${finColor} ${blanco}Corregir múltiples dispositivos? ${finColor}${turquesa}[Y/N]${finColor}${blanco}:${finColor} " && read sino
-		
-		#Condicional en el parámetro 1 para corregir 1 dispositivo o más (respuesta y/n)
-		if [[ "$sino" == "Y" || "$sino" == "y" ]]; then
-
-			#Preguntas datos dispositivos
-			echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el rango de la instalación [192.xxx.xx]: ${finColor}" && read rango
-			echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el Nº de Host de la 1º camara: ${finColor}" && read camara_1
-			echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el Nº de Host de la última camara: ${finColor}" && read camara_final
-			echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce la password de la instalacion: ${finColor}" && read password
-			echo -e "\n${blanco}-------------------------------------------------------------------${finColor}\n"
-			
-			#Corrección de datos
-			for camaras in $(seq $camara_1 $camara_final); do
-				
-				timeout 1 ping -c 1 $rango.$camaras >/dev/null
-
-				if [ "$?" -eq 0 ]; then
-					nombres="$(curl http://$rango.$camaras/ISAPI/System/deviceInfo --digest -u "admin:$password" -s | grep "<deviceName>" | sed 's/<deviceName>/[+] Camara: /' | sed 's/<\/deviceName>/- CORREGIDA/')"
-
-					curl -X POST -d "@xml_parameters/XML_NTPServer.xml" -H "Content-Type: application/xml" http://$rango.$camaras/ISAPI/System/time/ntpServers --digest -u "admin:$password" -s >/dev/null
-					echo -e "${verde}$nombres${finColor}" 				
-				else
-					echo -e "${rojo}[!] Camara: $rango.$camaras - OFFLINE${finColor}"
-				fi
-
-			done
-
-		elif [[ "$sino" == "N" || "$sino" == "n" ]]; then
-			
-			echo -en "\n${turquesa}[+]${finColor} ${blanco}Intoduce la IP del dispositivo: ${finColor}" && read ip
-			echo -en "${turquesa}[+]${finColor} ${blanco}Intoduce la password: ${finColor}" && read password
-			echo -e "\n${blanco}-------------------------------------------------------------------${finColor}\n"
-
-			timeout 1 ping -c 1 $ip >/dev/null
-
-			if [ "$?" -eq 0 ]; then
-				nombres="$(curl http://$ip/ISAPI/System/deviceInfo --digest -u "admin:$password" -s | grep "<deviceName>" | sed 's/<deviceName>/[+] Camara: /' | sed 's/<\/deviceName>/- CORREGIDA/')"
-				curl -X POST -d "@xml_parameters/XML_NTPServer.xml" -H "Content-Type: application/xml" http://$ip/ISAPI/System/time/ntpServers --digest -u "admin:$password" -s >/dev/null
-				echo -e "${verde}$nombres${finColor}" 				
-
-			else
-				echo -e "${rojo}[!] Camara: $ip - OFFLINE${finColor}"
-			fi
-		else
-			echo -e "\n${rojo}[!] ERROR [!]${finColor}"
-		fi
-
+		time_correct
 	else
 		echo -e "${rojo}[!] Número Incorrecto [!]${finColor}"
 	fi
@@ -353,9 +396,105 @@ function corrector(){
 
 }
 
+
+#-------------------------------------------------------------------------------------------
+#-----------------
+#MODO CONFIGURADOR
+#-----------------
+
+#-----------------
+#CONFIGURADOR CCTV
+#-----------------
+
+#-------------
+#MODELO SAFIRE
+#-------------
+
+function cctv_model_safire(){
+	
+	clear
+	
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el rango de la instalación [192.xxx.xxx]: ${finColor}" && read rango
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce cantidad de cámaras: ${finColor}" && read camaras
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce nombre de la instalación: ${finColor}" && read instalacion
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce la password de la instalacion: ${finColor}" && read password
+	echo -e "\n${blanco}-------------------------------------------------------------------${finColor}\n"
+	ip_final=$((16+$camaras-1))
+
+	echo -e "${amarillo}[!]${finColor} ${blanco}Actualizando Firmware...${finColor}\n"
+
+	for cameras in $(seq 16 $ip_final); do
+		timeout 1 ping -c 1 $rango.$cameras
+		if [ "$?" -eq 0 ]; then
+			echo "${turquesa}[+]${finColor} ${blanco}Camara: $cameras -${finColor} ${verde}ACTUALIZANDO${finColor}"
+			curl -X POST -# -O --upload-file "firmwares/SF-IPD835WA-8P-HV/digicap.dav" http://$rango.$cameras/ISAPI/System/updateFirmware --digest -u "admin:$password"
+			
+			curl -X PUT http://$rango.$cameras/ISAPI/System/reboot --digest -u "admin:$password" -s
+
+			ping 
+		else
+			echo "${rojo}[!]${finColor} ${blanco}Camara: $cameras -${finColor} ${rojo}OFFLINE${finColor}"
+
+		fi
+	done
+
+}
+
+#----------------
+#MODELO HIKVISION
+#----------------
+
+function cctv_model_hik(){
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce el rango de la instalación [192.xxx.xxx]: ${finColor}" && read rango
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce cantidad de cámaras: ${finColor}" && read camaras
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce nombre de la instalación: ${finColor}" && read instalacion
+	echo -en "\n${turquesa}[+]${finColor} ${blanco}Introduce la password de la instalacion: ${finColor}" && read password
+	echo -e "\n${blanco}-------------------------------------------------------------------${finColor}\n"
+
+
+}
+
+#---------------------
+#SELECTOR CONFIGURADOR
+#---------------------
+
+function configurator(){
+
+	clear
+
+	toilet Configurador -f pagga.tlf --metal
+
+	echo -e "\n${morado}[!]${finColor} ${blanco}¿Que quieres fabricar? ${finColor}${morado}[!]${finColor}\n"
+	
+	echo -e "\t${turquesa}[1]${finColor} ${blanco}CCTV${finColor}"
+	echo -e "\t${turquesa}[2]${finColor} ${blanco}Kit Portal${finColor}"
+	echo -e "\t${turquesa}[3]${finColor} ${blanco}Control Accesos${finColor}\n"
+
+	echo -en "${morado}[+]${finColor} ${blanco}Selecciona tipo de fabricación: ${finColor}" && read mode
+
+	if [ "$mode" -eq 1 ]; then
+		echo -e "\n${morado}[!]${finColor} ${blanco}Selecciona Modelo de Camara ${finColor}${morado}[!]${finColor}\n"
+		echo -e "\t${turquesa}[1]${finColor} ${blanco}SF-IPD835WA-8P-HV (Safire)${finColor}"
+		echo -e "\t${turquesa}[2]${finColor} ${blanco}DS-2CD2183G2-IU (HIKVISON)${finColor}"
+
+		echo -en "\n${morado}[+]${finColor} ${blanco}Introduce Número de Modelo: ${finColor}" && read model
+		if [ "$model" -eq 1 ]; then
+			cctv_model_safire
+		elif [ "$model" -eq 2 ]; then
+			cctv_model_hik
+		else
+			echo -e "${rojo}[!] Número Incorrecto [!]${finColor}"
+		fi
+	else
+		echo -e "${rojo}[!] Número Incorrecto [!]${finColor}"
+	fi
+}
+
 #-------------------------------------------------------------------------------------------
 
 #SELECCIÓN DE MODO
+
+clear
 
 toilet Laboratorio -f pagga.tlf --metal
 
@@ -379,6 +518,8 @@ elif [ "$mode" -eq 3 ]; then
 	revision_individual
 elif [ "$mode" -eq 4 ]; then
 	corrector
+elif [ "$mode" -eq 5 ]; then
+	configurator
 else
 	echo -e "${rojo}[!] Número Incorrecto [!]${finColor}"
 fi
